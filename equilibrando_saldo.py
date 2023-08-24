edbde5bf2aecf512a74cdf09da9493c959c53a1d
Juliane Ferreira <54564103+JulianeFerreira@users.noreply.github.com>
2023-08-23 23:48:25 -0300

    Delete equilibrando_saldo.py

--- equilibrando_saldo.py
+++
@@ -1,9 +0,0 @@
-saldo_atual = float(input())
-valor_deposito = float(input())
-valor_retirada = float(input())
-
-#TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
-saldo_atual += valor_deposito
-saldo_atual -= valor_retirada
-#TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
-print(f"Saldo atualizado na conta: {saldo_atual:.1f}")
