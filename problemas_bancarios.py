bdf2ea01ca68661ab5bd2b390092d1498d9ad00e
Juliane Ferreira <54564103+JulianeFerreira@users.noreply.github.com>
2023-08-23 23:49:15 -0300

    Delete problemas_bancarios.py

--- problemas_bancarios.py
+++
@@ -1,8 +0,0 @@
-valor = float(input())
-
-if valor > 0:
-    print(f"Deposito realizado com sucesso!\nSaldo atual: R$ {valor:.2f}")
-elif valor == 0:
-    print("Encerrando o programa...")
-else:
-    print("Valor invalido! Digite um valor maior que zero.")
